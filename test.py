20a5ceab74a790a02764734762fdbe0afa3b631f
HampusEriksson <hampuse1@gmail.com>
2022-08-12 11:33:43 +0200

    init

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("This is a test")
+klassen = ["Dante", "Rasmus", "Hundaol", "Lucia"]
+elev = input("Vem vill du kolla om de är med i klassen? ")
+
+if elev in klassen:
+   print(elev , "finns i klassen.")
+
+else:
+   print(elev , "finns inte i klassen.")
+
