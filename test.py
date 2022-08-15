df64ab97f37b2df8c3d8f04a7133e491b965af58
HampusEriksson <hampuse1@gmail.com>
2022-08-15 11:07:48 +0200

    travelbag

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-klassen = ["Dante", "Rasmus", "Hundaol", "Lucia"]
-elev = input("Vem vill du kolla om de är med i klassen? ")
-
-if elev in klassen:
-   print(elev , "finns i klassen.")
-
-else:
-   print(elev , "finns inte i klassen.")
-
+i = 0
+while True:
+   print(i)
+   i += 1
