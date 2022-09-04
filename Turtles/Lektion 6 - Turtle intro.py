975995ad27923ab6591699c17015f55206a9f37b
Hampus Eriksson <hampus.eriksson@ga.ntig.se>
2022-09-04 19:12:30 +0200

    Inför lektion 6

---
+++ Turtles/Lektion 6 - Turtle intro.py
@@ -0,0 +1,34 @@
+# För att använda turtle måste modulen importeras
+
+import turtle
+
+# Skapa en turtle
+
+adam = turtle.Turtle()
+
+# Ändra på turtle
+# Speed
+
+# Shape - “arrow”, “turtle”, “circle”, “square”, “triangle”, “classic”
+
+# Shapesize, pensize
+
+# Röra på turtle - forward, back, goto, setx, sety
+
+# Penup, pendown, pencolor
+
+# Hideturtle, showturtle
+
+# Rotera - right, left, seth
+
+# Circle (radie, vinkel, streck)
+
+# Dot (storlek, färg)
+
+# Få data från turtle . pos, xcor, ycor, heading
+
+# Distance (a,b)
+
+
+# Skriv turtle.done() för att fönstret ska vara kvar öppet
+turtle.done()
