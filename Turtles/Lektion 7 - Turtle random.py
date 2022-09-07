91091378f5b4d469c4f5397506b0b406805f8762
HampusEriksson <hampuse1@gmail.com>
2022-09-07 10:15:48 +0200

    Lektion 7

--- Turtles/Lektion 7 - Turtle random.py
+++ Turtles/Lektion 7 - Turtle random.py
@@ -1,1 +1,14 @@
 import turtle, random
+
+benny = turtle.Turtle()
+benny.shapesize(10)
+
+benny.forward(random.randint(50,200))
+
+benny.color(random.random(), random.random(), random.random())
+
+colors = ["red", "green"]
+
+benny.color(random.choice(colors))
+
+turtle.done()
--- Turtles/Lektion 7 - randomness.py
+++ Turtles/Lektion 7 - randomness.py
@@ -1,9 +1,26 @@
 import random
 
 # Random integer
+a = random.randint(1,50)
+print(a)
 
 # Random float - uniform
+b = random.uniform(1,50)
+print(b)
 
 # Random between 0-1
+c = random.random()
+print(c)
+
+# Slump med procent
+if c > 0.9:
+    print("You were very lucky.")
 
 # Random letters
+name = "sebastian vettel"
+
+print(random.choice(name))
+
+# Random choice
+students = ["Diana", "Rasmus", "Alexander"]
+print("Den som får A i programmering är: ", random.choice(students))
