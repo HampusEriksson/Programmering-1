0e511dd62ba1ac486c99dc4c0af7a97b4048d7aa
HampusEriksson <hampuse1@gmail.com>
2022-09-08 08:30:31 +0200

    RPS prepp

---
+++ Övningar/Övning 1 - RPS.py
@@ -0,0 +1,9 @@
+import random
+
+computer = random.choice(["rock", "paper", "scissors"])
+
+user = input("rock, paper or scissors? ")
+
+print("The computer chose", computer,"and the user chose", user +".")
+
+# TODO - Implement the if statements that prints who wins
