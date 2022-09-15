0a100941c32e7baa14e9b008b8b16c9e546bb741
HampusEriksson <hampuse1@gmail.com>
2022-09-15 08:51:27 +0200

    Lektion 11 + rps facit

--- Arrays/Lektion 11 - Dictionary.py
+++ Arrays/Lektion 11 - Dictionary.py
@@ -1,23 +1,69 @@
 # Tidigare genomgång: https://www.youtube.com/watch?v=e3LL4mSrLMs&ab_channel=HampusEriksson
 
+# Users
 # Skapa dictionary
-
 # Key-value pair
 
+users = {"alec.andin" : "1234", "eddie.sunden" : "nocap", "adam" : "adam123", "berit" : "berit123", "carl":"carl123"}
+
+print(*users)
+
 # Lägg till key-value pair
+users["dante.norling"] = "aaahh"
+print(*users)
 
 # Ändra value
+users["dante.norling"] = "surprised look"
 
 # Ta bort - del
+del users["dante.norling"]
 
 # Ta bort - pop
+users.pop("alec.andin")
+
+print(*users)
+
+"""
+logged_in = False
+
+while not logged_in:
+
+    username = input("Username: ")
+    password = input("Password: ")
+
+    if users[username] == password:
+        print("Logged in.")
+        logged_in = True
+
+    else:
+        print("Username or password is incorrect.")
+
+"""
 
 # .keys()
+for x in users.keys():
+    print(x)
 
 # .values()
+for x in users.values():
+    print(x)
 
 # .items()
-
-# Users
+for x, y in users.items():
+    print(x, y)
 
 # Räkna
+import random
+
+counts = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+
+for _ in range(1000):
+    slump = random.randint(1,6)
+    counts[slump] += 1
+
+print(counts)
+
+
+
+
+
--- Övningar/RPS - facit.py
+++ Övningar/RPS - facit.py
@@ -1,3 +1,155 @@
+# Basic - 9 if
+import random
+
+computer = random.choice(["rock", "paper", "scissors"])
+
+user = input("rock, paper or scissors? ")
+
+print("The computer chose", computer,"and the user chose", user +".")
+
+# TODO - Implement the if statements that prints who wins
+
+if user == "rock" and computer == "paper":
+    print("computer wins")
+
+elif user == "paper" and computer == "scissors":
+    print("computer wins")
+
+elif user == "rock" and computer == "scissors":
+    print("user wins")
+
+elif user == "paper" and computer == "rock":
+    print("user wins")
+
+elif user == "rock" and computer == "rock":
+    print("tie ")
+
+elif computer == "scissors" and user == "paper":
+    print("computer wins")
+
+elif computer == "paper" and user == "paper":
+    print("tie")
+
+elif computer == "paper" and user == "scissors":
+    print("user wins")
+
+elif user == "scrissors" and computer == "scissors":
+    print("tie ")
+
+# Lika med => tie - 7 if
+
+import random
+
+computer = random.choice(["rock", "paper", "scissors"])
+
+user = input("rock, paper or scissors? ")
+
+print("The computer chose", computer,"and the user chose", user +".")
+if user==computer:
+    print("Draw play again.")
+elif computer == ("rock") and user == ("paper"):
+    print("user wins")
+elif computer ==("paper") and user == ("scissors"):
+    print("user wins")
+elif computer ==("scissors") and user ==("rock"):
+    print("user wins")
+elif user == ("rock") and computer == ("paper"):
+    print("big L")
+elif user ==("paper") and computer == ("scissors"):
+    print("Big L")
+elif user ==("scissors") and computer ==("rock"):
+    print("Big L")
+
+# Lika som ovan fast med and
+import random
+
+computer = random.choice(["rock", "paper", "scissors"])
+
+user = input("rock, paper or scissors? ")
+
+print("The computer chose", computer,"and the user chose", user +".")
+
+# TODO - Implement the if statements that prints who wins
+
+if computer == "rock" and user == "scissors" or computer == "paper" and user == "rock" or computer == "scissors" and user == "paper":
+    print("Computer wins!")
+elif computer == "scissors" and user == "rock" or computer == "rock" and user == "paper" or computer == "paper" and user == "scissors":
+    print("User wins!")
+else:
+    print("Draw!")
+
+# 5 if
+import random
+
+computer = random.choice(["rock", "paper", "scissors"])
+
+user = input("rock, paper or scissors? ")
+
+print("The computer chose", computer,"and the user chose", user +".")
+
+# TODO - Implement the if statements that prints who wins
+
+if user == "rock" and computer == "scissors": 
+    print ("Win")
+
+
+elif user == "paper" and computer == "rock": 
+    print ("Win")
+
+
+elif user == "scissors" and computer == "paper": 
+    print ("Win")
+
+
+elif user == computer:
+    print ("draw")
+
+else:
+    print("lose")
+
+
+
+# Sista elevlösningen
+import random
+
+while True:
+
+    computer = random.choice(["rock", "paper", "scissors"])
+
+    user = input("rock, paper or scissors?\n")
+
+
+    while True:
+        
+        if user == computer:
+            print(f"You took {user} and the computer took {computer} so it is a draw you suck bozo ")
+
+        elif user == "rock":
+            if computer == "scissors":
+                print("You won!")
+                
+            else:
+                print("You lost!")
+               
+        elif user == "scissors":
+            if computer == "paper":
+                print("You won!")
+                
+            else:
+                print("You lost!")
+                
+
+        elif user == "paper":
+            if computer == "rock":
+                print("You won!")
+                
+            else:
+                print("You lost!")
+                
+
+        break
+
+# Index - 3 if
 import random
 
 possible_choices = ["rock", "paper", "scissors", "fire", "lizard"]
