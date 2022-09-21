a566e95307284f8da6aebf0f35ecd1b1bf3ab173
HampusEriksson <hampuse1@gmail.com>
2022-09-21 10:28:19 +0200

    Lektion 13

--- Funktioner/Lektion 13 - quiz exempel utan funktioner.py
+++ Funktioner/Lektion 13 - quiz exempel utan funktioner.py
@@ -1,2 +1,38 @@
+name = input("Name: ")
+
+while True:
+    score = 0
+    difficulty = input("Easy, hard or quit? ").lower()
+
+    if difficulty == "easy":
+        print("Welcome to the easy quiz.")
+        ans1 = input("What is the capital of England? ").lower()
+        if ans1 == "london":
+            print("Correct")
+            score += 1
+
+        else:
+            print("Incorrect")
+
+        print(f"Quiz over, you got {score} points.")
+
+
+    elif difficulty == "hard":
+        print("Welcome to the hard quiz.")
+        ans1 = input("Whats the meaning of life? ").lower()
+        if ans1 == "42":
+            print("Correct")
+            score += 1
+
+        else:
+            print("Incorrect")
+
+        print(f"Quiz over, you got {score} points.")
+
+    elif difficulty == "quit":
+        break
+
+    else:
+        print("Not a valid option")
 
 
--- test.py
+++ test.py
@@ -1,7 +1,30 @@
-import turtle
+score = 0
 
-adam = turtle.Turtle()
+ögon = input("vilken färg är dina ögon?").capitalize()
+if ögon == ("Brun"):
+    print ("helt rätt")
+    score +=5
+else: print("tyvärr fel")
 
-adam.forward(50)
+huvudstad = input("vad är sveriges huvudstad?").capitalize()
+if huvudstad == ("Stockholm"):
+    print ("helt rätt")
+    score +=5
+else: print("tyvärr fel")
 
-turtle.done()
+gymnasiet = input("vilket gymnasiet går du?").capitalize()
+if gymnasiet == ("Nti"):
+    print ("helt rätt")
+    score +=5
+else: print("tyvärr fel")
+
+mat = input("vad är din favorit mat?").capitalize()
+if mat == ("Pizza"):
+    print ("helt rätt")
+    score +=5
+else: print("tyvärr fel.")
+
+poäng = input("vill du se dina poäng?").capitalize()
+if poäng == ("Ja"):
+    
+    print ("Hej", score)
