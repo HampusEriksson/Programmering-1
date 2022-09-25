50457d7121bd0241315cdd29eb4bfc5b87e4ee2d
Hampus Eriksson <hampus.eriksson@ga.ntig.se>
2022-09-25 16:40:42 +0200

    Lektion 15 prepp

---
+++ Print, input, variabler, datatyper, operatorer/Lektion 15 - Fördjupning.py
@@ -0,0 +1,24 @@
+# Print
+pass
+
+# Två parametrar
+
+# Format
+
+# Citattecken
+
+# Multi-line str
+
+# Input
+
+# Int - input
+
+# Ny rad i str
+
+# Variabel
+
+# String
+
+# Heltal
+
+# Kommentarer
