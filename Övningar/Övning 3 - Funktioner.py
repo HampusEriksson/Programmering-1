3675d168cfb7d4ace3a501b80bab5aa20a3bd289
Hampus Eriksson <hampus.eriksson@ga.ntig.se>
2022-09-25 16:25:48 +0200

    Övning 3 prepp

---
+++ Övningar/Övning 3 - Funktioner.py
@@ -0,0 +1,42 @@
+# Övning 3 går ut på att implementera så många av nedanstående funktioner som möjligt
+# Ta bort pass och implementera funktionerna
+
+
+def best(name):
+    # TODO Skriv ut att namnet är bäst
+    # Ex: "Katherine" in - skriver ut "Katherine är bäst"
+    pass
+
+
+def square(number):
+    # TODO Returnera talet kvadrerat
+    # Ex: 5 in - 25 ut
+    pass
+
+
+def sums(a, b):
+    # TODO Returnera summan av a och b
+    # Ex: 2, 6 in - 8 ut
+    pass
+
+
+# Nu blir det lite svårare
+
+
+def maximum(a, b, c):
+    # TODO Returnera det största av a,b,c
+    # Ex: 3, 6, 12 in - 12 ut
+    pass
+
+
+def palindrom(ord):
+    # TODO Returnera True om ord är ett palindrom
+    # Ex: abba in - True ut
+    # Palindrom är ett ord som stavas likadant baklänges och framlänges.
+    pass
+
+
+def prime(nr):
+    # TODO Returnera True om nr är ett primtal, annars returnera false
+    # Ex: 5 in - True ut
+    pass
