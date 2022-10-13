762bedc65fa556fbc36623c0fd2cba5a9fc97441
HampusEriksson <hampuse1@gmail.com>
2022-10-13 12:55:43 +0200

    Lektion 19

--- Kontrollstrukturer/Lektion 17 - If else fördjupning.py
+++ Kontrollstrukturer/Lektion 17 - If else fördjupning.py
@@ -1,16 +1,64 @@
 # Booleans 
+# Kan vara True eller False
+# Jämförelser har värdet True eller False
+import time
 
-# If else
 
-# Elif
+print(5 > 5)
+
+# If else elif
+name = input("Name? ").capitalize()
+
+if name == "Orvar":
+    print("Hej")
+
+elif name == "Lion":
+    print("Print ariful")
+
+elif name == "Andhrew tate":
+    print("Usch")
+
+else:
+    print("Inget fall fanns")
 
 # and
-
 # or
-
+age = 5
+if age == 5 and name == "Peter":
+    print("nånting")
+"""
 # if else i variabeldeklaration
+active_player = "X"
 
+while True:
+    time.sleep(1)
+    # Här körs en runda
+    if active_player == "X":
+        active_player = "Y"
+
+    elif active_player == "Y":
+        active_player = "X"
+
+active_player = "X"
+
+while True:
+    time.sleep(1)
+    # Här körs en runda
+    active_player = "X" if active_player == "Y" else "Y"
+
+"""
 # match case
 
-# Fizz buzz exempel
+name = input("Name? ").capitalize()
+
+match name:
+
+    case "Orvar":
+        print("Hej orvar")
+
+    case "Oskar":
+        print("Hej oskar")
+
+
+
 
--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
-values = ["Ace", "King", "Queen", "Jack", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-deck = []
-for suit in suits:
-     for value in values:
-          deck.append(Card(value + " of " + suit, value))
+
+
+print(12 // 5)
