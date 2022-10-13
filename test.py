ddc1fab5688bcf32ba941b0a4271e2c9d88acc91
HampusEriksson <hampuse1@gmail.com>
2022-10-13 09:11:01 +0200

    Lektion 18

--- test.py
+++ test.py
@@ -1,27 +1,6 @@
-score =0
-fot = input ("vilket lag hejar Mohammed?").capitalize
-if fot == ("real madrid"):
-    print ("rätt som fan")
-    score +=3
-else: 
-    print ("tyvärr fel")
-
-spelare = input ("vem är världens bästa spelare enligt mohammed?").capitalize
-if spelare == ("Benzema"):
-    score +=3
-    print ("korrekt")
-else: 
-    print ("tyvärr fel")
-
-Elit = input("Hur många Ballon d'Or har ronaldo vunnit?").capitalize
-if Elit == ("5"):
-    print ("korrekt")
-    score +=3
-else: print ("tyvärr fel")
-
-mål = input("hur många mål har Benzema gjort under 2021/2022 säsongen?")
-if mål == ("100 mål"):
-    print ("helt rätt")
-    score +=3
-
-else: print ("tyvärr fel")
+suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
+values = ["Ace", "King", "Queen", "Jack", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+deck = []
+for suit in suits:
+     for value in values:
+          deck.append(Card(value + " of " + suit, value))
