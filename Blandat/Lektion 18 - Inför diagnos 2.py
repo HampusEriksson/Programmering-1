75dbc68b482465632c788677771219f4b1c38a42
HampusEriksson <hampuse1@gmail.com>
2022-10-13 12:32:22 +0200

    Lektion 19 - Inför diagnos 2

---
+++ Blandat/Lektion 18 - Inför diagnos 2.py
@@ -0,0 +1,28 @@
+# Datatyper
+# String
+"En string är en text"
+# Int - heltal
+1337
+# Float - decimaltal
+13.37
+# Boolean - True / False
+True
+# Addition med olika datatyper
+# print(7+"5.7")
+
+# Operatorer - + - / * - upphöjt = **
+
+# Modulo
+# Floor division - dela och sen runda ner
+
+apples = int(input("How many apples do you have? "))
+people = int(input("How many people are you guys? "))
+
+print(f"You get {apples // people} apples each.")
+print(f"You have {apples % people} apples left.")
+
+# Konvertera
+a = "15"
+b = 100
+a = int(a)
+c = a+b
