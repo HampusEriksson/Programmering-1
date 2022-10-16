c758be2a6280973a1ba2b7bf0233cd2dd258cab1
Hampus Eriksson <hampus.eriksson@ga.ntig.se>
2022-10-16 14:59:08 +0200

    Update Lektion 20 - Dictionary fördjupning.py

--- Arrays/Lektion 20 - Dictionary fördjupning.py
+++ Arrays/Lektion 20 - Dictionary fördjupning.py
@@ -1,1 +1,33 @@
-pass
+# Tidigare genomgång: https://www.youtube.com/watch?v=e3LL4mSrLMs&ab_channel=HampusEriksson
+
+# Users
+# Skapa dictionary
+# Key-value pair
+
+users = {
+    "alec.andin": "1234",
+    "eddie.sunden": "nocap",
+    "adam": "adam123",
+    "berit": "berit123",
+    "carl": "carl123",
+}
+
+print(*users)
+
+# Lägg till key-value pair - kolla först om den finns!
+
+# Ändra value - kolla först om den finns!
+
+# Ändra value med update - kolla först om den finns!
+users.update({"Hampus": 5})
+
+# Ta bort - del
+
+# Ta bort - pop
+
+
+# .keys()
+
+# .values()
+
+# .items()
