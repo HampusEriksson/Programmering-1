5bb372d6c55e2c81f6fcfcbfaeed44c71686b334
HampusEriksson <hampus.eriksson@ga.ntig.se>
2023-08-22 12:56:41 +0200

    L1 done

--- HT23/Modul 1/L1 - Print, input, variabler, datatyper.py
+++ HT23/Modul 1/L1 - Print, input, variabler, datatyper.py
@@ -1,16 +1,37 @@
 # Variabler
-
-# Öka värdet
+# Citattecken = string = text
+name = "Tom"
+# integer = int = hel
+score = 0
 
 # print
+print("Välkommen till programmet!")
+
+# Öka värdet
+score += 1
+
+# Likvärdigt med:
+score = score + 1
+
+score = 100
+
+score -= 7
+
 
 # input - sparar som str
+# \n ger en ny rad i en sträng, ska vara innanför ""
+name2 = input("What is your name?\n")
+
 
 # print - format
+print(f"Välkommen till programmet {name2} och {name}!")
+
+print(f"5 plus 7 är lika med {5+7}")
 
 # Datatyper
 # str - string - text
-
+"Hampus"
 # int - integer - heltal
-
+-29
 # float - decimaltal
+29.5
