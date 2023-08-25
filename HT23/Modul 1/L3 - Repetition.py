2964606d1fe33c2ae066449c6dce2aa0087263ab
HampusEriksson <hampus.eriksson@ga.ntig.se>
2023-08-25 12:27:59 +0200

    Create L3 - Repetition.py

---
+++ HT23/Modul 1/L3 - Repetition.py
@@ -0,0 +1,14 @@
+# variabel
+my_pet = "Musse"
+my_age = 29
+my_weight = 84.3
+
+# input
+your_name = input("What is your name?\n")
+
+# input till heltal
+your_age = int(input("What is your age?"))
+
+# string - text
+# int - heltal
+# float - decimaltal
