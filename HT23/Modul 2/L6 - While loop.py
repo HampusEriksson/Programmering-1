357f0dd540d58bf5186a20a873f7abe722eea96f
HampusEriksson <hampus.eriksson@ga.ntig.se>
2023-08-30 09:53:16 +0200

    L6 done

--- HT23/Modul 2/L6 - While loop.py
+++ HT23/Modul 2/L6 - While loop.py
@@ -1,7 +1,46 @@
 # While True
-
+secret_word = "gustav vasa"
+while True:
+    svar = input("Vem vill du kyssa?")
+    if svar == secret_word:
+        break
+    else:
+        print("Wrong")
+    
 
 # While condition
+svar =""
+while svar != secret_word:
+    svar = input("Vem vill du kyssa?")
+
+# Login
+user = "hampus1"
+password = "hej123"
+
+user_input = input("What is your username?")
+user_password = input("What is your password?")
+
+while user != user_input and password != user_password:
+    print("Password or username is incorrect.")
+
+    user_input = input("What is your username?")
+    user_password = input("What is your password?")
+
+
 
 # Continue
-# Login
+# continue can help streamline code and avoid 
+# unnecessary processing when certain conditions are met.
+count = 0
+
+while count < 5:
+    count += 1
+    
+    if count == 3:
+        print("Skipping iteration", count)
+        continue
+    
+    print("Iteration", count)
+
+print("Loop finished")
+
