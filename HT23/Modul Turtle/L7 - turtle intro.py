51e15697fd8d5ffa8786fe7bebbe6eb826a7c157
Hampus Eriksson <hampus.eriksson@ga.ntig.se>
2023-09-03 18:43:29 +0200

    L7 prepp

--- HT23/Modul Turtle/L7 - turtle intro.py
+++ HT23/Modul Turtle/L7 - turtle intro.py
@@ -2,80 +2,39 @@
 import turtle
 
 # Skapa en turtle
-adam = turtle.Turtle()
 
 # Ändra på turtle
 # Speed - 1 till 10, men 0 är snabbast
-adam.speed(0)
 
 # Shape - “arrow”, “turtle”, “circle”, “square”, “triangle”, “classic”
-adam.shape("turtle")
 
 # Shapesize, pensize
-adam.shapesize(5)
-adam.pensize(5)
-adam.forward(100)
 
-# Röra på turtle - forward, back, goto, setx, sety
-adam.forward(100)
-adam.back(50)
-adam.goto(-350,350)
-adam.sety(-150)
+# Röra på turtle - .forward, .back, .goto, .setx, .sety
 
-# Penup, pendown, pencolor
-# Sluta rita
-adam.penup()
+# Penup, pendown,
+# Sluta rita - .penup
 
-# Börja rita igen
-adam.pendown()
-# Byt med text
-adam.pencolor("red")
+
+# Börja rita igen - .pendown
+
+
+# Byt med text - .pencolor
 
 # Byt med rgb
-adam.pencolor(1, 0, 0)
-adam.forward(100)
 
-# Hideturtle, showturtle
-adam.hideturtle()
-adam.showturtle()
+# .hideturtle(), .showturtle()
 
-# Rotera - right, left, seth
-adam.right(90)
-adam.seth(132)
+# Rotera - .right, .left, .seth
 
 # Circle (radie, vinkel, streck)
-adam.circle(75)
-
-adam.circle(125, 180)
-
-adam.circle(200, 270, 15)
 
 # Dot (storlek, färg)
-adam.dot(100, "black")
 
-# Få data från turtle . pos, xcor, ycor, heading
-adam.setx(500)
-if adam.xcor() > 400:
-    adam.setx(0)
+# Få data från turtle .pos, .xcor, .ycor, .heading
 
-adam.pos()
-adam.ycor()
-adam.heading()
 
 # Distance (a,b)
-harry = turtle.Turtle()
-harry.shape("turtle")
-harry.shapesize(5)
-harry.pensize(5)
-harry.goto(100, 150)
-
-print(turtle.distance(adam, harry))
-
-while True:
-    adam.right(7)
-    adam.forward(50)
-    if abs(adam.xcor()) > 400 or abs(adam.ycor()) > 400:
-        adam.goto(0,0)
 
 # Skriv turtle.done() för att fönstret ska vara kvar öppet
 turtle.done()
