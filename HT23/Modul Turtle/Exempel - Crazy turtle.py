b39a886f952a1c5fe305cd005b4abcc6c97be70d
HampusEriksson <hampus.eriksson@ga.ntig.se>
2023-09-05 12:36:20 +0200

    L8 done

--- HT23/Modul Turtle/Exempel - Crazy turtle.py
+++ HT23/Modul Turtle/Exempel - Crazy turtle.py
@@ -10,10 +10,12 @@ while True:
     adam.right(random.randint(1,15))
     adam.forward(random.randint(10,30))
     adam.color(random.random(), random.random(), random.random())
+    adam.dot(25, (random.random(),random.random(),random.random()))
 
     if abs(adam.xcor()) > 400 or abs(adam.ycor()) > 400:
         adam.penup()
         adam.goto(0,0)
         adam.pendown()
+        
 
 turtle.done()
