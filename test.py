2239cbe4601564e5cf3d3256146ba58a370b8d50
HampusEriksson <hampus.eriksson@ga.ntig.se>
2023-09-13 15:46:29 +0200

    L12 done

--- test.py
+++ test.py
@@ -1,1 +1,30 @@
-"That's it"
+# i = loopvariabel
+for ok in range(10, 23, 3):
+    print(ok**2)
+tries = 0
+while True:
+    namn = input("Vad heter du? ")
+    if namn == "Alex":
+        print("Hej Alex!")
+        break
+    else:
+        print("Hej", namn, "!")
+        tries += 1
+    
+    if tries == 3:
+        print("Du har försökt för många gånger!")
+        break
+        
+
+tries = 0
+while tries < 3:
+    namn = input("Vad heter du? ")
+    if namn == "Alex":
+        print("Hej Alex!")
+        
+    else:
+        print("Hej", namn, "!")
+        tries += 5
+        
+
+print("Här är vi ute från while loopen!")
