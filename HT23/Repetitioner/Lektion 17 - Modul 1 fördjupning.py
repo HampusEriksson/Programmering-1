a5f630bc2d6ad641d6e0386803496be12aa1158a
HampusEriksson <hampus.eriksson@ga.ntig.se>
2023-09-26 12:47:12 +0200

    Lektion 18 done

--- HT23/Repetitioner/Lektion 17 - Modul 1 fördjupning.py
+++ HT23/Repetitioner/Lektion 17 - Modul 1 fördjupning.py
@@ -1,33 +1,52 @@
 # Print
+print("Hello World")
 
 # Print - format
+namn = "Kalle"
+meddelande = f"Välkommen {namn}."
+print(meddelande)
+print(f"Välkommen {namn}.")
+
+# Print, med ,
+print("Välkommen", namn,".")
 
 # Print - sep och end
+print("Välkommen", namn,".", sep="-", end="")
 
 # Print - citattecken
+print('"Kalle"')
 
 # Print - multi-line
+print("""Hej välkommen till
+min multi-line""")
 
 # Print - ny rad
+print("Hej\nvälkommen\ntill\nmin\nmulti-line")
 
 # Input
+namn = input("Vad heter du? ")
 
 # Int - input
+age = int(input("Hur gammal är du? "))
 
-# Variabel
+age2 = input("Hur gammal är du? ")
+age2 = int(age2)
 
 # String - text
-
+"Vilgot"
 
 # Heltal - int
-
+5
 
 # Decimaltal - float
+5.7
 
 # Kommentarer - förklarar programmet
 
-# Användaren skriver in sin ålder med input och konverterar till int. Sparas i age
 
 
 # 3 citattecken är multiline kommentar
 
+"""
+Detta är en multiline kommentar
+Hej"""
---
+++ HT23/Repetitioner/Lektion 18 - Prepp inför diagnos 1.py
@@ -0,0 +1,75 @@
+# För att skriva ut på skärmen behöver du använda en specifik funktion. Vad heter den? Hur fungerar den?
+print()
+# Funktionen som hänvisas till i fråga 1 har två parametrar vi kan ändra på om vi vill. Vilka är dessa? (Alltså de Hampus har nämnt, det finns mer än 2.)
+print("Hej", sep=" ", end="\n")
+# Vad är en sträng?
+# En sträng är en text
+
+# Vi kan skriva strängar på två sätt i Python. Vilka då?
+str1 = "Kerolos"
+str2 = """
+Kerolos
+Yahya
+"""
+# Om du ska skriva ett stort stycke text finns ett särskilt sätt att skriva. Hur gör du det?
+"""
+"""
+# När du ska ta in data från användaren, vilken funktion använder du då?
+input()
+
+# Vilken variabeltyp ger funktionen som hänvisas till i fråga 6 som standard?
+# string
+
+# Hur kan du ändra variabeltypen som funktionen som hänvisas till i fråga 6 ger?
+age = int(input("Hur gammal är du? "))
+
+# Om du vill göra ny rad i en text, vad skriver du då?
+str3 = "Mattias\nYahya"
+
+# Vad är en variabel?
+# Ett värde på något man har gjort, som man vill spara
+# T.ex. namnge en låda och spara ett värde i den
+
+# Hur definieras en variabel som sträng?
+name = "Kerolos"
+
+# Hur definieras en variabel som ett heltal?
+age = 18
+
+# Varför föredras formatsträngar (dvs f-strings)?
+# Mer frihet och lättare att formatera
+print(f"Jag heter {name} och är {age} år gammal.")
+
+# Vad är en kommentar i Python?
+# Underlättar för den som läser koden eller för den som kodar
+# Vilka två sätt kan man göra kommentarer på?
+
+# Det här är en kommentar
+"""
+Det här är en kommentar
+Också"""
+# Hur gör du en kommentar i Python?
+
+# Hur hanterar datorn dina kommentarer?
+# Datorn ignorerar kommentarer när koden körs
+
+# Beskriv vad följande variabeltyper används till och ge exempel på vad de kan innehålla:
+# string(str) - text
+# integer(int) - heltal
+# float - decimaltal
+
+# Skriv tecknet för operationen:
+# addition +
+# subtraktion -
+# division /
+# multiplikation *
+# floor division //
+print(17 // 5)
+# modulo %
+# power to/upphöjt i **
+
+# Vilken datatyp blir det i slutändan om du adderar en float och en integer?
+# float
+# Vilken datatyp blir det i slutändan om du använder division?
+# float när vi använder division
+print(15 / 5)
