4f5941b4b4fb5d4a85774201a99eab19459ddd57
Hampus Eriksson <hampus.eriksson@ga.ntig.se>
2023-10-02 20:40:11 +0200

    renames

---
+++ HT23/Repetitioner/Lektion 19 - Psuedokod exempel.py
@@ -0,0 +1,11 @@
+# Ta in nummer från användaren
+
+# Om talet är mindre än 1, skriv ut "inte primtal" och avsluta programmet
+
+# Om talet är 1 eller 2, skriv ut "primtal" och avsluta programmet
+
+# Om talet är större än 2, kolla om det är jämnt delbart med något tal mellan 2 och talet-1
+
+# Så fort vi hittar ett tal som är delbart med talet, skriv ut "inte primtal" och avsluta programmet
+
+# Om vi inte hittar något tal som är delbart med talet, skriv ut "primtal" och avsluta programmet
--- HT23/Repetitioner/Lektion 25 - Funktioner fördjupning lambda.py
+++ HT23/Repetitioner/Lektion 25 - Funktioner fördjupning lambda.py
@@ -1,15 +1,19 @@
 # Import my_functions
 
 from my_functions import check_list
+
+
 # https://www.w3schools.com/python/python_functions.asp
 # Skapa funktion
 def f(x):
     y = x * 3 + 70
     return y
 
+
 def f2(x):
     y = x * 3 + 70
     print(y)
+
 
 f15 = f(15)
 print(f15)
@@ -20,31 +24,31 @@ print(f(127))
 # docstring
 
 
-
 mobiler = ["iPhone 14", "One plus", "Blackberry", "Samsung 22"]
 
 check_list(mobiler)
 check_list("fiasgufoasgo")
 
 
-def func(x,y):
+def func(x, y):
     return x + y
 
-print(func(5,13))
+
+print(func(5, 13))
 
 # Lambda
 # variabelnamn = lambda parameter1, parameter2 : returdata
-func2 = lambda x,y : x+y
+func2 = lambda x, y: x + y
 
-print(func2(5,13))
+print(func2(5, 13))
 
 
 # sortera list av tuples
-students = [ ('Erik', 15), ('Adam', 17), ('Berit', 13), ('Carl', 20), ('David', 10)]
+students = [("Erik", 15), ("Adam", 17), ("Berit", 13), ("Carl", 20), ("David", 10)]
 
 # Sorterar default på index [0]
 students.sort()
 
 # Om vi vill sortera på andra index, använd lambda-funktion
-students.sort(key = lambda x : x[1])
+students.sort(key=lambda x: x[1])
 print(students)
