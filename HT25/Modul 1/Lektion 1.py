eb18c3b63fd7ceb7e02f99707cb616ad0af5f542
HampusEriksson <90573586+HampusEriksson@users.noreply.github.com>
2025-08-26 10:50:34 +0200

    L1 done

--- HT25/Modul 1/Lektion 1.py
+++ HT25/Modul 1/Lektion 1.py
@@ -1,12 +1,41 @@
+# Datatyper - str - text
+"Hampus"
+# Citattecken = shift + 2
 
+# Datatyper - int - heltal
+51965
 
-# Variabler
-
-
+# Datatyper - float - decimaltal
+218956.498316
 
 # print
+print("Välkommen till programmet!")
+print(17)
+print(531.312)
+print(5+5)
+
+# Variabler  
+
+username = "Noob13"
+print(username)
+username = "NoobKiller64"
+print(username)
+
+score = 0
+print(score)
+
+score += 1
+print(score)
 
 # input
+username = input("Username: ")
+password = input("Password: ")
 
+print(username)
+print(password)
 
+# formattera string
+# {} alt gr + 7
+print(f"Välkommen till programmet {username}!")
 
+print("Välkommen till programmet username!")
---
+++ HT25/Modul 1/Lektion 2.py
@@ -0,0 +1,19 @@
+# Omvandla input till int
+
+# Addition
+
+# Subtraktion
+
+
+# Multiplikation
+
+# Division
+
+
+# Floor division
+
+# Upphöjt i
+
+
+# Modulo
+
