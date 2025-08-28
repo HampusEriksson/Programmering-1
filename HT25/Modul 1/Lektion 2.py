fffea66380cf7de72722bb0748d065201ed605d5
HampusEriksson <90573586+HampusEriksson@users.noreply.github.com>
2025-08-28 09:24:37 +0200

    Update Lektion 2.py

--- HT25/Modul 1/Lektion 2.py
+++ HT25/Modul 1/Lektion 2.py
@@ -1,19 +1,31 @@
 # Omvandla input till int
+# age = int(input("What is your age?"))
+
+
+# print(f"Om 10 år är du {age+10} år gammal.")
+
+a = 5
+b = 7
 
 # Addition
-
+print(a+b)
 # Subtraktion
-
+print(a-b)
 
 # Multiplikation
-
+print(a*b)
 # Division
-
+print(a/b)
+# Upphöjt i
+print(5**7)
 
 # Floor division
-
-# Upphöjt i
-
+print(37 // 3)
 
 # Modulo
+print(37 % 5)
 
+# Udda tal
+print(3 % 2)
+# Jämna tal
+print(8 % 2)
