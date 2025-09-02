2e818ccb1db7243ac0a38cc1283a4703ec21cbb4
HampusEriksson <90573586+HampusEriksson@users.noreply.github.com>
2025-09-02 16:06:53 +0200

    L3 done

---
+++ HT25/Modul 1/Lektion 3.py
@@ -0,0 +1,41 @@
+# Datatyper: str, int, float
+
+# str - string - text
+name = "Hampus"
+# int - integer - heltal
+age = 31
+
+# float - decimaltal
+length = 1.80
+
+# print(): skriva ut
+print("Hej TEI24A!")
+
+# f-string
+print(f"{name} är {age} år gammal och är {length} m lång.")
+
+# input(): skriva in
+food = input("What is your favorite food? ")
+print(f"{food} is your favorite food.")
+
+# Beräkningar: +, -, *, /
+
+print(2+2)
+print(5-3)
+print(5*3)
+print(8/2)
+
+# ** = upphöjt i
+print(5**3)
+
+# ** 0.5 = roten ur
+print(9**(0.5))
+
+# // floor division
+print(38//3)
+
+# Typkonvertering: str(), int(), float()
+your_age = int(input("What is your age? "))
+
+print(f"{your_age*2} is your double age.")
+
