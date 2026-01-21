042316abda88dd60a1bc32a5dc3ba952f70db9c0
HampusEriksson <90573586+HampusEriksson@users.noreply.github.com>
2026-01-21 16:19:58 +0100

    Lektion 4

---
+++ HT25/Modul 2/Lektion 4.py
@@ -0,0 +1,36 @@
+# Booleans
+# En datatyp
+# 2 möjliga värden
+True
+False
+
+# Skriva ut boolean 
+print(5 > 7)
+
+age = int(input("How old are you?"))
+
+# if else
+if (age < 12):
+    print("Du har långt kvar till bilen")
+elif (age < 18):
+    print("Du får inte köra bil.")
+elif (age >= 75):
+    print("Du är för gammal.")
+else:
+    print("Du får övningsköra.")
+
+# .lower, .upper, .capitalize
+glass = input("Vill du ha glass?")
+glass = glass.lower()
+
+if (glass == "ja"):
+    print("Här får du glass.")
+else:
+    print("Du får inte glass.")
+
+# or - exempel 1
+
+
+# and
+
+
