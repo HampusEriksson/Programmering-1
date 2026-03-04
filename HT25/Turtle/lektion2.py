cc1dd0d8e18a665e93a295fe68ba37d703df455a
HampusEriksson <90573586+HampusEriksson@users.noreply.github.com>
2026-03-04 16:02:26 +0100

    Update lektion2.py

--- HT25/Turtle/lektion2.py
+++ HT25/Turtle/lektion2.py
@@ -1,6 +1,26 @@
-import turtle
+import turtle, random
 
-# .hideturtle(), .showturtle()
+padda = turtle.Turtle()
+padda.shapesize(5)
+padda.shape("turtle")
+padda.penup()
+padda.speed(0)
+turtle.screensize(1200,800)
+
+apple = turtle.Turtle()
+apple.color("red")
+apple.penup()
+apple.goto(random.randint(-500, 500), random.randint(-500, 500))
+
+while True:
+    padda.forward(10)
+    if padda.xcor() >= 600:
+        padda.hideturtle()
+        padda.goto(-600,random.randint(-500, 500))
+        padda.showturtle()
+    if padda.distance(apple) < 50:
+        break
+
 
 # Få data från turtle .pos, .xcor, .ycor, .heading
 # Distance (a,b)
