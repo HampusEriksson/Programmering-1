be540fc0c21ced1d854d9ca81df128b1e1b4fd18
HampusEriksson <90573586+HampusEriksson@users.noreply.github.com>
2026-03-05 12:10:58 +0100

    Update lektion3.py

--- HT25/Turtle/lektion3.py
+++ HT25/Turtle/lektion3.py
@@ -5,7 +5,8 @@ screen = turtle.Screen()
 screen.setup(width=800, height=600)
 padda = turtle.Turtle()
 padda.speed(0)
-riktning = "up"
+padda.shapesize(7)
+stopped = False
 
 # Turtle för score
 score_turtle = turtle.Turtle()
@@ -24,9 +25,10 @@ def uppdatera_score():
 
 # Funktioner som bara ändrar variabeln 'riktning'
 def ga_upp():
-    padda.seth(90)
+    padda.setheading(90)
 
 def ga_ner():
+    # seth är samma som setheading
     padda.seth(270)
 
 def ga_vanster():
@@ -35,20 +37,34 @@ def ga_vanster():
 def ga_hoger():
     padda.seth(0)
 
+def stop():
+    global stopped
+    """if stopped == False:
+        stopped = True
+    else:
+        stopped = False"""
+    # Samma sak som if-satsen ovan, men på en rad
+    stopped = not stopped
+
+
 # Koppla tangenter
 screen.listen()
 screen.onkeypress(ga_upp, "Up")
 screen.onkeypress(ga_ner, "Down")
 screen.onkeypress(ga_vanster, "Left")
 screen.onkeypress(ga_hoger, "Right")
+screen.onkeypress(stop, "space")
+
 
 # Huvudloopen som sköter rörelsen
 def flytta():
-   
-    padda.forward(5)
+
+    if stopped == False:
+        padda.forward(5)
     
     # Kör denna funktion igen efter 20 millisekunder
     screen.ontimer(flytta, 20)
 
 flytta() # Starta rörelsen
+
 screen.mainloop()
