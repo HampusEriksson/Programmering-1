3dfd9b0e8b21dccebe7d2708be196676433b1591
Hampus Eriksson <hampuse1@gmail.com>
2026-03-23 07:15:40 +0100

    L20 prepp

---
+++ HT25/Modul 3/Lektion 20 - Prepp inför diagnos.py
@@ -0,0 +1,58 @@
+# Hur vet datorn vilken kod som ska köras när en if-sats blir sann?
+
+
+# Vad heter datatypen som är relevant när vi pratar om if-satser och jämförelser?
+
+
+# Vilka värden kan datatypen i fråga 2 anta?
+
+
+# Vilka jämförelseoperatorer och logiska operatorer har vi i python?
+
+
+# I vilken ordning prioriteras de logiska operatorerna?
+
+
+# If-satser kan inte vara tomma, vad skriver vi för att göra koden körbar ändå?
+
+
+# Hur är metoder som .lower(), upper() och .capitalize() relevant när vi pratar om if-satser? Förklara och/eller ge exempel
+
+
+# Beskriv skillnaden mellan if, elif och else.
+
+
+# När används elif?
+
+
+# Loopar
+
+
+# Beskriv kort skillnaden på en while- och en for-loop.
+
+
+# Vad kallas variabeln du skapar i en for-loop?
+
+
+# Vilka datastrukturer kan vi for-loopa igenom (iterera över)?
+
+
+# Vad gör range-funktionen?
+
+
+# Vad blir skillnaden på range om du har en, två eller tre parametrar?
+
+
+# Vad gör att while-loopar fungerar? Vad krävs?
+
+
+# Vad gör nyckelordet continue?
+
+
+# Vad gör nyckelordet break?
+
+
+# När kontrolleras villkoret i en while-loop?
+
+
+# Hur fungerar for-else? Dvs else kopplat till en for-loop
